4aa77d830ea0e9f5dec585062fca23336020dad4
Ogbu Goodnews <ogbugoodnews74@gmail.com>
2026-06-20 12:23:47 -0700

    Add files via upload

--- train.py
+++ train.py
@@ -1,8 +1,11 @@
 import joblib
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from xgboost import plot_importance
 from sklearn.ensemble import IsolationForest
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from xgboost import XGBClassifier
 from sklearn.metrics import classification_report, roc_auc_score
 
@@ -19,16 +22,14 @@ X = df[features]
 
 y = df["is_fraud"]
 
-# Split the dataset into 98.5% for training and 20% for testing
+# Split the dataset into 80% for training and 20% for testing
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.2,  random_state=42 )
 
 # Anomaly Detection Layer (Isolation Forest)
-# We flag things that deviate from the normal distribution
 iso = IsolationForest(contamination= 0.015, random_state= 42)
 iso_pred = iso.fit_predict(X_train)
 
 # XGBoost learns to distinguish fraud from legitimate traffic
-
 neg_count = y_train.value_counts()[0]
 pos_count = y_train.value_counts()[1]
 
@@ -43,11 +44,27 @@ model = XGBClassifier(
 
 model.fit(X_train, y_train)
 
+# Features Importance
+fig1, ax1 = plt.subplots(figsize=(10, 6))
+plot_importance(model, max_num_features=5, importance_type='weight', ax=ax1)
+plt.title("Top Fraud Indicators")
+plt.savefig("feature_importance.png") 
+plt.close(fig1)
+
 # Evaluation
 preds = model.predict(X_test)
+
 print("Model Classification")
 print(classification_report(y_test, preds))
 print(f"ROC-AUC Score: {roc_auc_score(y_test, preds):.4f}")
 
 joblib.dump(model, "fraud_detection_model.pkl")
 print("Model training Completed")
+
+fig2, ax2 = plt.subplots(figsize=(8, 6))
+cm = confusion_matrix(y_test, preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=['Legit', 'Fraud'])
+disp.plot(cmap=plt.cm.Blues, ax=ax2)
+plt.title("Fraud Detection Performance")
+plt.savefig("confusion_matrix.png")
+plt.close(fig2)
